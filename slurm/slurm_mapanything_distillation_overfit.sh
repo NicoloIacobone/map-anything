d253aef45622a1ab27ffa06511c2af3f80ea3220
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-11-26 17:52:20 +0100

    test case - trying to run overfit with batch_size = 12

--- slurm/slurm_mapanything_distillation_overfit.sh
+++ slurm/slurm_mapanything_distillation_overfit.sh
@@ -75,7 +75,7 @@ echo "Detected $NUM_GPUS GPUs: $CUDA_VISIBLE_DEVICES"
 
 python distillation_overfit.py \
   --epochs 5000 \
-  --batch_size 4 \
+  --batch_size 12 \
   --lr 1e-4 \
   --num_workers 0 \
   --print_freq 500 \
