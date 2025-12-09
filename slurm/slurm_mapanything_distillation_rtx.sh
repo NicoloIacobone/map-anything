1dbdd58a708fb7c40e4c2296e81667f6c8015e03
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2025-12-09 15:17:37 +0100

    test case - MV_04

--- slurm/slurm_mapanything_distillation_rtx.sh
+++ slurm/slurm_mapanything_distillation_rtx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 # Specify job name.
-#SBATCH --job-name=MV_03
+#SBATCH --job-name=MV_04
 #
 # Specify output file.
-#SBATCH --output=MV_03_%j.log
+#SBATCH --output=MV_04_%j.log
 #
 # Specify error file.
-#SBATCH --error=MV_03_%j.err
+#SBATCH --error=MV_04_%j.err
 #
 # Specify open mode for log files.
 #SBATCH --open-mode=append
@@ -72,10 +72,10 @@ torchrun --nproc_per_node=$NUM_GPUS distillation.py \
   --use_wandb \
   --num_workers 8 \
   --multi_view_mode \
-  --wandb_name "MV_03" \
+  --wandb_name "MV_04" \
   --epochs 100 \
   --lr 1e-3 \
-  --max_views 16 \
+  --max_views 32 \
   --lr_scheduler none \
   --eval_freq 1 \
   --save_freq 10 \
