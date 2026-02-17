add6bfc9c46daeefc31a82e23fcd41552a095557
NICOLO' IACOBONE s317025 <nicolo.iacobone@gmail.com>
2026-02-17 12:25:18 +0100

    test case - Thank_You

---
+++ slurm/thank_you.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Specify job name.
+#SBATCH --job-name=Thank_You
+#
+# Specify output file.
+#SBATCH --output=Thank_You_%j.log
+#
+# Specify error file.
+#SBATCH --error=Thank_You_%j.err
+#
+# Specify open mode for log files.
+#SBATCH --open-mode=append
+#
+# Specify time limit.
+#SBATCH --time=00:10:00
+#
+# Specify number of tasks.
+#SBATCH --ntasks=1
+#
+# Specify number of CPU cores per task.
+#SBATCH --cpus-per-task=8
+#
+# Specify memory limit per CPU core.
+#SBATCH --mem-per-cpu=4096
+#
+# Specify number of required GPUs.
+#SBATCH --gpus=rtx_4090:1
+#
+# Specify disk limit on local scratch.
+#SBATCH --tmp=500000
+#
+# Specify email notifications.
+# #SBATCH --mail-type=BEGIN,END,FAIL
+# #SBATCH --mail-user=niacobone@student.ethz.ch
+
+echo "=== Job starting on $(hostname) at $(date) ==="
+# DATE_VAR=$(date +%Y%m%d%H%M%S)
+
+# Load modules.
+module load stack/2024-06 python/3.12 cuda/12.4 eth_proxy
+echo "Loaded modules: $(module list 2>&1)"
+
+# Activate virtual environment for sam2.
+source /cluster/scratch/niacobone/map-anything/myenv/bin/activate
+echo "Activated Python venv: $(which python)"
+
+# Execute
+cd /cluster/scratch/niacobone/map-anything/scripts
+echo "Starting MapAnything distillation..."
+
+python extract_pca_visualization.py \
+    --checkpoint_path /cluster/work/igp_psr/niacobone/distillation/output/SV_03_new/checkpoints/checkpoint_best.pth \
+    --image_path /cluster/work/igp_psr/niacobone/distillation/Thank_You.jpg \
+    --output_dir ./output_pca
+
+echo "=== Job finished at $(date) ==="
+start_time=${SLURM_JOB_START_TIME:-$(date +%s)}
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
+echo "Total execution time: $(printf '%02d:%02d:%02d\n' $((elapsed/3600)) $(( (elapsed%3600)/60 )) $((elapsed%60))) (hh:mm:ss)"
